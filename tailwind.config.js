cc9a8a475ec2d859c475b646ad511b9bcb1a7924
Lewis Howles <lewishowles@users.noreply.github.com>
2024-08-10 00:20:33 +0100

    Extend the default Tailwind config
    Adding colours, dark mode strategy, and sensible plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,29 @@
+import colors from "tailwindcss/colors";
+
 export default {
+	darkMode: "selector",
 	content: [
 		"./index.html",
 		"./src/**/*.vue",
 	],
 	theme: {
-		extend: {},
+		colors: {
+			// Set our preferred colours from those provided by Tailwind
+			transparent: "transparent",
+			current: "currentColor",
+			black: colors.black,
+			white: colors.white,
+			grey: colors.slate,
+			red: colors.red,
+			orange: colors.orange,
+			yellow: colors.amber,
+			green: colors.green,
+			teal: colors.teal,
+			blue: colors.sky,
+			indigo: colors.indigo,
+			purple: colors.purple,
+			pink: colors.pink,
+		},
 	},
 	plugins: [],
 };
