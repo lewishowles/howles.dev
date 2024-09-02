0800d93f60f676f834111cc31c2a1d41b3381e35
Lewis Howles <lewishowles@users.noreply.github.com>
2024-09-02 13:31:43 +0100

    `create-project`: Add mock success status

--- src/components/welcome-bento/fragments/create-project/create-project.vue
+++ src/components/welcome-bento/fragments/create-project/create-project.vue
@@ -31,14 +31,32 @@
 				</ui-button>
 			</form-actions>
 		</form-layout>
+
+		<transition
+			enter-active-class="transition ease-out duration-500"
+			enter-from-class="opacity-0 transform -translate-y-4"
+			enter-to-class="opacity-100 transform translate-y-0"
+			leave-active-class="transition ease-in duration-500"
+			leave-from-class="opacity-100 transform translate-y-0"
+			leave-to-class="opacity-0 transform translate-y-4"
+		>
+			<div v-show="showingSuccessMessage" class="absolute inset-x-0 top-0 mx-1 mt-1 flex items-center justify-center gap-2 rounded-xl bg-gradient-to-br from-green-500 to-green-600 p-6 text-white">
+				<icon-check-circled class="size-6" />
+
+				{{ t("create_project.form.success") }}
+			</div>
+		</transition>
 	</div>
 </template>
 
 <script setup>
 import { reactive, ref } from "vue";
+import { runComponentMethod } from "@lewishowles/helpers/vue";
 import { useI18n } from "vue-i18n";
+import useApi from "@/composables/use-api";
 
 const { t, tm } = useI18n();
+const { delay } = useApi();
 // A reference to the submit button, allowing us to reset it after project
 // creation or on error.
 const submitButton = ref(null);
@@ -51,10 +69,42 @@ const project = reactive({
 	type: defaultType.value,
 });
 
+// Whether to display a success message.
+const showingSuccessMessage = ref(false);
+
 /**
  * Simulate a new project being created.
  */
-function createProject() {
+async function createProject() {
+	try {
+		await delay(1000);
 
+		runComponentMethod(submitButton.value, "reset");
+
+		showSuccessMessage();
+	} catch {
+		console.log("create-project[createProject]: Failed to create project");
+	}
+}
+
+/**
+ * Show a sample success message and reset the form.
+ */
+async function showSuccessMessage() {
+	showingSuccessMessage.value = true;
+
+	setTimeout(() => {
+		showingSuccessMessage.value = false;
+
+		resetForm();
+	}, 2000);
+}
+
+/**
+ * Reset our form to default values.
+ */
+function resetForm() {
+	project.name = null;
+	project.type = defaultType.value;
 }
 </script>
--- src/composables/use-api.js
+++ src/composables/use-api.js
@@ -72,6 +72,7 @@ export default function useApi() {
 	}
 
 	return {
+		delay,
 		isLoading,
 		isReady,
 		lastRunTime,
