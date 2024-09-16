3fd6b8bea0dccd197d067b8f85af37fa1123d985
Lewis Howles <lewishowles@users.noreply.github.com>
2024-09-16 12:13:03 +0100

    fix: Hard-coded colours in icon-robot

--- src/components/icon/icon-robot.vue
+++ src/components/icon/icon-robot.vue
@@ -4,9 +4,9 @@
 			<path d="M0 0h18v18H0z" />
 			<rect width="13.5" height="11.25" x="2.25" y="3.938" fill="currentColor" fill-opacity="0.1" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" rx="1.5" />
 			<rect width="7.875" height="2.813" x="5.063" y="10.125" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" rx="1" />
-			<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10.421 10.125v2.813m-2.812-2.813v2.813m1.406-9V1.125" />
-			<circle cx="5.906" cy="7.594" r="1" fill="#BE185D" fill-rule="nonzero" />
-			<circle cx="12.094" cy="7.594" r="1" fill="#BE185D" fill-rule="nonzero" />
+			<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M10.421 10.125v2.813m-2.812-2.813v2.813m1.406-9V1.125" />
+			<circle cx="5.906" cy="7.594" r="1" fill="currentColor" fill-rule="nonzero" />
+			<circle cx="12.094" cy="7.594" r="1" fill="currentColor" fill-rule="nonzero" />
 		</g>
 	</base-icon>
 </template>
