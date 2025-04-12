3306fa40e98a32547bf1a868902c8cf83a471a52
Lewis Howles <lewishowles@users.noreply.github.com>
2025-04-12 14:39:10 +0100

    Add subtle border animation to bento boxes

--- src/components/welcome-bento/fragments/bento-box/bento-box.vue
+++ src/components/welcome-bento/fragments/bento-box/bento-box.vue
@@ -1,11 +1,17 @@
 <template>
-	<div class="rounded-2xl border border-grey-300 shadow-sm transition-colors dark:border-transparent dark:bg-grey-950/20 dark:shadow-none" :class="paddingClasses" data-test="bento-box">
-		<slot />
+	<div class="bento-border flex h-full flex-col rounded-2xl p-px">
+		<div class="grow rounded-2xl border border-grey-300 hover:border-transparent focus-within:border-transparent bg-white shadow-sm dark:border-transparent dark:bg-[#380A32] dark:shadow-none" :class="[attrs.class, paddingClasses]" data-test="bento-box">
+			<slot />
+		</div>
 	</div>
 </template>
 
 <script setup>
 import { computed, useAttrs } from "vue";
+
+defineOptions({
+	inheritAttrs: false,
+});
 
 const attrs = useAttrs();
 
@@ -29,3 +35,41 @@ const paddingClasses = computed(() => {
 	return paddingClasses;
 });
 </script>
+
+<style>
+.bento-border {
+	--angle: 0deg;
+
+	&:hover, &:focus-within {
+		background-image: conic-gradient(
+			from var(--angle),
+			var(--color-grey-200) 10%,
+			var(--color-pink-600),
+			var(--color-grey-200) 20%
+		);
+
+		animation: 6s rotate linear infinite;
+
+		&:where(.dark, .dark *) {
+			background-image: conic-gradient(
+				from var(--angle),
+				rgba(0, 0, 0, 0) 10%,
+				var(--color-pink-300),
+				rgba(0, 0, 0, 0) 20%
+			);
+		}
+	}
+}
+
+@keyframes rotate {
+	to {
+		--angle: 360deg;
+	}
+}
+
+@property --angle {
+	syntax: '<angle>';
+	initial-value: 0deg;
+	inherits: false;
+}
+</style>
